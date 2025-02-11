0643cf8691d1a9220738c9b503a09551e2bed449
mfwolffe <wolffemf@dukes.jmu.edu>
2025-02-11 17:41:47 -0500

    initial main

--- daisy.py
+++ daisy.py
@@ -37,4 +37,17 @@ def setup_logger(debug):
     """ init logger for daisy cli """
     level = logging.DEBUG if debug else logging.INFO
     logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=level)
-    
+
+
+def main():
+    args = parse_cli()
+    setup_logger(args.debug)
+
+    config        = load_config(args.config)
+    volume        = args.volume if args.volume is not None else config["volume"]
+    flicker_speed = args.flicker_speed if args.flicker_speed is not None else config["flicker_speed"]
+    voice         = args.voice if args.voice else config["voice"]
+
+    logging.info(f"Using config - Volume: {volume}, Flicker Speed: {flicker_speed}, Voice: {voice}")
+
+
