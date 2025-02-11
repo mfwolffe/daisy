2670df4830a39449a315a219c0e2a073e8946b39
mfwolffe <wolffemf@dukes.jmu.edu>
2025-02-11 16:05:51 -0500

    defaults

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+Config for daisy
+While the project is entirely a joke, I'm planning to package
+As such, things should be FHS compliant
+"""
+
+
+import os
+import yaml
+import logging
+
+SYSTEM_CONFIG_PATH = "/etc/daisy/config.yaml"
+USER_CONFIG_PATH = os.path.expanduser("~/.config/daisy/config.yaml")
+
+DEFAULT_CONFIG = {
+  # TODO @mfwolffe deb install or rules needs this path def
+  #
+  #
+  "audio_file": "/usr/share/daisy/audio/daisy.mp3",
+  "volume": 1.0,
+  "flicker_speed": None,
+  # NOTE: voice synthesis is default mode;
+  #       fallback is recording of the IBM 7094
+  #
+  "voice": "synth",
+}
