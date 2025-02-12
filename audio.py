57a64a6a5605a1020258ab042b728db7f67ff521
mfwolffe <wolffemf@dukes.jmu.edu>
2025-02-11 19:17:39 -0500

    quick refactor

--- audio.py
+++ audio.py
@@ -28,6 +28,42 @@ LYRICS = [
 
 LINES = len(LYRICS)
 
+
+def apply_low_pass(audio, cutoff_freq):
+    """
+      Applies a low-pass filter to simulate HAL's muffled shutdown
+
+      The lower the `cutoff_freq`, the more muffled and distant HAL’s voice sounds.
+      examples:
+          500   (Hz) -> no effect
+          3000  (Hz) -> slightly muffled
+          1500  (Hz) -> Noticeably muffled
+          800   (Hz) -> Very muffled, depending on sample probably inaudible
+    """
+    return audio.low_pass_filter(cutoff_freq)
+
+
+def apply_glitch_effect(audio, intensity=0.2):
+    """ 
+        Randomly introduces distortion effects to mimic HAL’s failing voice 
+
+        Does so by reducing sample rate to yield glitch effects, causing HAL to 
+        exude instability
+        Higher values of `intensity` create larger corruptions
+
+        examples:
+            0.0 -> No effect
+            0.1 -> Mild, occasional glitch
+            0.3 -> Frequent distortion
+            0.7 -> severe degradation
+            1.0 -> heavy corruption; likely incomprehensible
+    """
+    if random.random() < 0.2:
+        logging.debug("Applying glitch effect")
+        return audio.set_frame_rate(int(audio.frame_rate * (1 - intensity)))
+    return audio
+
+
 # NOTE: not sure how it'll fare performance wise with
 #       the voice synthesis
 #
@@ -55,40 +91,6 @@ def process_raw(audio, decay_factor):
 
     audio = apply_glitch_effect(audio, intensity=decay_factor)
 
-    return audio
-
-
-def apply_low_pass(audio, cutoff_freq):
-    """
-      Applies a low-pass filter to simulate HAL's muffled shutdown
-
-      The lower the `cutoff_freq`, the more muffled and distant HAL’s voice sounds.
-      examples:
-          500   (Hz) -> no effect
-          3000  (Hz) -> slightly muffled
-          1500  (Hz) -> Noticeably muffled
-          800   (Hz) -> Very muffled, depending on sample probably inaudible
-    """
-    return audio.low_pass_filter(cutoff_freq)
-
-def apply_glitch_effect(audio, intensity=0.2):
-    """ 
-        Randomly introduces distortion effects to mimic HAL’s failing voice 
-
-        Does so by reducing sample rate to yield glitch effects, causing HAL to 
-        exude instability
-        Higher values of `intensity` create larger corruptions
-
-        examples:
-            0.0 -> No effect
-            0.1 -> Mild, occasional glitch
-            0.3 -> Frequent distortion
-            0.7 -> severe degradation
-            1.0 -> heavy corruption; likely incomprehensible
-    """
-    if random.random() < 0.2:
-        logging.debug("Applying glitch effect")
-        return audio.set_frame_rate(int(audio.frame_rate * (1 - intensity)))
     return audio
 
 
@@ -138,9 +140,6 @@ def daisy_daisy(volume=1.0, voice="synth"):
         engine = pyttsx3.init()
         engine.setProperty("volume", volume)
 
-        # TODO: @mfwolffe apply processing 
-        #
-        #
 
         for line in LYRICS:
             engine.say(line)
